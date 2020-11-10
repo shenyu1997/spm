1be82de04f7ee4dfbc5b6403a75c21445cc65b31
ShenYu <yu.shen@microfocus.com>
2020-11-10 08:11:11 +0800

    US, expose DATE-REST by annotation

--- src/main/java/tech/kuiperbelt/spm/config/SpmRepositoryRestConfigurer.java
+++ src/main/java/tech/kuiperbelt/spm/config/SpmRepositoryRestConfigurer.java
@@ -3,9 +3,13 @@ package tech.kuiperbelt.spm.config;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.core.mapping.ExposureConfiguration;
+import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import tech.kuiperbelt.spm.common.BeforeCreateOrSaveBaseEntityValidator;
+import tech.kuiperbelt.spm.domain.core.Phase;
 
 @Setter
 @Configuration
@@ -18,5 +22,10 @@ public class SpmRepositoryRestConfigurer implements RepositoryRestConfigurer {
     public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
         validatingListener.addValidator("beforeCreate", beforeCreateOrSaveBaseEntityValidator);
         validatingListener.addValidator("beforeSave", beforeCreateOrSaveBaseEntityValidator);
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.setRepositoryDetectionStrategy(RepositoryDetectionStrategy.RepositoryDetectionStrategies.ANNOTATED);
     }
 }
