ddb07d02b73da369bed12dc28f06c89b70096ec8
ShenYu <yu.shen@microfocus.com>
2020-11-11 13:37:15 +0800

    US, output error to error log

--- src/main/java/tech/kuiperbelt/spm/common/ExceptionHandlerAdvice.java
+++ src/main/java/tech/kuiperbelt/spm/common/ExceptionHandlerAdvice.java
@@ -3,6 +3,7 @@ package tech.kuiperbelt.spm.common;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.rest.webmvc.RepositoryRestExceptionHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +14,13 @@ import tech.kuiperbelt.spm.SpmApplication;
 
 import java.util.Optional;
 
+@Slf4j
 @RestControllerAdvice(basePackageClasses = {SpmApplication.class, RepositoryRestExceptionHandler.class})
 public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorMessage> handle(IllegalArgumentException ex) {
+        log.error("error: {}", Optional.ofNullable(ex.getMessage()).orElse(ex.getClass().getSimpleName()), ex);
         return ResponseEntity.badRequest()
                 .body(ErrorMessage.builder()
                         .message(ex.getMessage())
@@ -29,6 +32,7 @@ public class ExceptionHandlerAdvice {
     public ResponseEntity<ErrorMessage> handle(Exception ex) {
         String message = Optional.of(ex.getMessage())
                 .orElse(ex.getClass().getSimpleName());
+        log.error("error: {}", message, ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ErrorMessage.builder()
                         .message(message)
