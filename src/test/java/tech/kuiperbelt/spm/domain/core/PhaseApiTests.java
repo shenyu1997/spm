ae6d19f536866e2ee3369a0eda6caadc6c0f3530
ShenYu <yu.shen@microfocus.com>
2020-11-19 13:18:35 +0800

    UT, PhaseApiTests: getProjectPhases, starProjectToDoneProject

--- src/test/java/tech/kuiperbelt/spm/domain/core/PhaseApiTests.java
+++ src/test/java/tech/kuiperbelt/spm/domain/core/PhaseApiTests.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.security.test.context.support.WithMockUser;
 import tech.kuiperbelt.spm.support.ApiTest;
 
+import java.time.LocalDate;
+
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsIterableContaining.hasItems;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -23,21 +26,71 @@ public class PhaseApiTests extends ApiTest {
     @Test
     public void getProjectPhases() throws Exception {
         String projectHref = testUtils.createRandomProject();
-
+        String firstPhasesHref = testUtils.appendRandomPhase(projectHref, LocalDate.now(), LocalDate.now().plusDays(10));
+        String secondPhaseHref = testUtils.appendRandomPhase(projectHref, LocalDate.now().plusDays(20));
+        mockMvc.perform(get(projectHref + "/phases"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.phases.length()", equalTo(2)))
+                .andExpect(jsonPath("$._embedded.phases..self.href",
+                        hasItems(firstPhasesHref, secondPhaseHref)));
     }
 
     @Test
-    public void startPhaseByStartProject() throws Exception {
-        //TODO
+    public void starProjectToDoneProject() throws Exception {
+        String projectHref = testUtils.createRandomProject();
+        String firstPhasesHref = testUtils.appendRandomPhase(projectHref, LocalDate.now(), LocalDate.now().plusDays(10));
+        String secondPhaseHref = testUtils.appendRandomPhase(projectHref, LocalDate.now().plusDays(20));
+
+        // 1. Start project
+        mockMvc.perform(post(projectHref + "/actions/start"))
+                .andExpect(status().isNoContent());
+
+        // Verify project and phase status
+        mockMvc.perform(get(projectHref))
+                .andExpect(jsonPath("$.canBeDone", equalTo(false)));
+
+        mockMvc.perform(get(firstPhasesHref))
+                .andExpect(jsonPath("$.status", equalTo(RunningStatus.RUNNING.name())));
+
+        mockMvc.perform(get(secondPhaseHref))
+                .andExpect(jsonPath("$.status", equalTo(RunningStatus.INIT.name())));
+
+        // 2. Done first phase
+        mockMvc.perform(post(firstPhasesHref + "/actions/done"))
+                .andExpect(status().isNoContent());
+
+        // Verify project and phase status
+        mockMvc.perform(get(projectHref))
+                .andExpect(jsonPath("$.canBeDone", equalTo(false)));
+
+        mockMvc.perform(get(firstPhasesHref))
+                .andExpect(jsonPath("$.status", equalTo(RunningStatus.STOP.name())));
+
+        mockMvc.perform(get(secondPhaseHref))
+                .andExpect(jsonPath("$.status", equalTo(RunningStatus.RUNNING.name())));
+
+        // 3. Done second phase
+        mockMvc.perform(post(secondPhaseHref + "/actions/done"))
+                .andExpect(status().isNoContent());
+
+        // Verify project and phase status
+        mockMvc.perform(get(projectHref))
+                .andExpect(jsonPath("$.canBeDone", equalTo(true)));
+
+        mockMvc.perform(get(secondPhaseHref))
+                .andExpect(jsonPath("$.status", equalTo(RunningStatus.STOP.name())));
+
+        // 4. Done project
+        mockMvc.perform(post(projectHref + "/actions/done"))
+                .andExpect(status().isNoContent());
+
+        mockMvc.perform(get(projectHref))
+                .andExpect(jsonPath("$.status", equalTo(RunningStatus.STOP.name())));
+
     }
 
     @Test
     public void startPhaseAutomaticallyAfterFirstInsertRunningProject() throws Exception {
-        //TODO
-    }
-
-    @Test
-    public void donePhaseAutomaticallyStartNextPhaseAndImpactProjectIsCanBeDone() throws Exception {
         //TODO
     }
 
