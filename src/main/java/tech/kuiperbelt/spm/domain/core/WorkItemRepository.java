d3a04acb5bc38b1c134f5e5bf8bf90977603b774
ShenYu <yu.shen@microfocus.com>
2020-11-23 21:44:05 +0800

    US, WorkItemRepository.findMyItems

--- src/main/java/tech/kuiperbelt/spm/domain/core/WorkItemRepository.java
+++ src/main/java/tech/kuiperbelt/spm/domain/core/WorkItemRepository.java
@@ -3,6 +3,7 @@ package tech.kuiperbelt.spm.domain.core;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
@@ -19,5 +20,8 @@ public interface WorkItemRepository extends JpaRepository<WorkItem, Long> {
     Page<WorkItem> findByAssignee(@Param("assignee") String assignee, Pageable pageable);
 
     Page<WorkItem> findByOwner(@Param("owner") String owner, Pageable pageable);
+
+    @Query("from WorkItem w where w.owner=:me or w.assignee=:me")
+    Page<WorkItem> findMyItems(@Param("me") String me, Pageable pageable);
 
 }
