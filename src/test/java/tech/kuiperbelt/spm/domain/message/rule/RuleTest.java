9db14096a577a36ae08bb989f2ef329aff3e5df2
ShenYu <yu.shen@microfocus.com>
2020-12-14 11:22:49 +0800

    UT, RuleTest

--- src/test/java/tech/kuiperbelt/spm/domain/message/rule/RuleTest.java
+++ src/test/java/tech/kuiperbelt/spm/domain/message/rule/RuleTest.java
@@ -3,9 +3,16 @@ package tech.kuiperbelt.spm.domain.message.rule;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import tech.kuiperbelt.spm.domain.core.Phase;
+import tech.kuiperbelt.spm.domain.core.Project;
+import tech.kuiperbelt.spm.domain.core.RunningStatus;
+import tech.kuiperbelt.spm.domain.core.WorkItem;
 import tech.kuiperbelt.spm.domain.core.event.Event;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RuleTest {
 
@@ -91,6 +98,263 @@ class RuleTest {
         Event event3 = Event.builder().triggeredMan(RandomStringUtils.randomAlphanumeric(10)).build();
         assertFalse(rule3.evaluate(event3, upn, null, null, null));
 
+    }
+
+    @Test
+    public void evaluateEventArgs() {
+        Rule rule = Rule.builder().eventArgs(1).build();
+        Event event = Event.builder().args(RandomStringUtils.randomAlphanumeric(10), upn).build();
+        assertTrue(rule.evaluate(event, upn, null, null, null));
+
+        Rule rule2 = Rule.builder().eventArgs(2).build();
+        Event event2 = Event.builder().args(RandomStringUtils.randomAlphanumeric(10), upn).build();
+        assertFalse(rule2.evaluate(event2, upn, null, null, null));
+    }
+
+    @Test
+    public void evaluateProjectOwner () {
+        Rule rule1 = Rule.builder().isProjectOwner(true).build();
+        Rule rule2 = Rule.builder().build();
+        Rule rule3 = Rule.builder().isProjectOwner(false).build();
+
+        Project project1 = new Project().toBuilder()
+                .owner(upn)
+                .build();
+
+        Project project2 = new Project().toBuilder()
+                .owner(RandomStringUtils.randomAlphanumeric(10))
+                .build();
+
+        Event event = Event.builder()
+                .build();
+
+        assertTrue(rule1.evaluate(event, upn, null, null, project1));
+        assertTrue(rule2.evaluate(event, upn, null, null, project1));
+        assertFalse(rule3.evaluate(event, upn, null, null, project1));
+
+        assertFalse(rule1.evaluate(event, upn, null, null, project2));
+        assertTrue(rule2.evaluate(event, upn, null, null, project2));
+        assertTrue(rule3.evaluate(event, upn, null, null, project2));
+    }
+
+    @Test
+    public void evaluateProjectManager () {
+        Rule rule1 = Rule.builder().isProjectManager(true).build();
+        Rule rule2 = Rule.builder().build();
+        Rule rule3 = Rule.builder().isProjectManager(false).build();
+
+        Project project1 = new Project().toBuilder()
+                .manager(upn)
+                .build();
+
+        Project project2 = new Project().toBuilder()
+                .manager(RandomStringUtils.randomAlphanumeric(10))
+                .build();
+
+        Event event = Event.builder()
+                .build();
+
+        assertTrue(rule1.evaluate(event, upn, null, null, project1));
+        assertTrue(rule2.evaluate(event, upn, null, null, project1));
+        assertFalse(rule3.evaluate(event, upn, null, null, project1));
+
+        assertFalse(rule1.evaluate(event, upn, null, null, project2));
+        assertTrue(rule2.evaluate(event, upn, null, null, project2));
+        assertTrue(rule3.evaluate(event, upn, null, null, project2));
+    }
+
+    @Test
+    public void evaluateProjectMembers () {
+        Rule rule1 = Rule.builder().belongToProjectMember(true).build();
+        Rule rule2 = Rule.builder().build();
+        Rule rule3 = Rule.builder().belongToProjectMember(false).build();
+
+        Project project1 = new Project().toBuilder()
+                .members(Collections.singletonList(upn))
+                .build();
+
+        Project project2 = new Project().toBuilder()
+                .members(Collections.singletonList(RandomStringUtils.randomAlphanumeric(10)))
+                .build();
+
+        Event event = Event.builder()
+                .build();
+
+        assertTrue(rule1.evaluate(event, upn, null, null, project1));
+        assertTrue(rule2.evaluate(event, upn, null, null, project1));
+        assertFalse(rule3.evaluate(event, upn, null, null, project1));
+
+        assertFalse(rule1.evaluate(event, upn, null, null, project2));
+        assertTrue(rule2.evaluate(event, upn, null, null, project2));
+        assertTrue(rule3.evaluate(event, upn, null, null, project2));
+    }
+
+    @Test
+    public void evaluateProjectParticipants () {
+        Rule rule1 = Rule.builder().belongToProjectParticipant(true).build();
+        Rule rule2 = Rule.builder().build();
+        Rule rule3 = Rule.builder().belongToProjectParticipant(false).build();
+
+        Project project1 = new Project().toBuilder()
+                .members(Collections.singletonList(upn))
+                .build();
+
+        Project project2 = new Project().toBuilder()
+                .members(Collections.singletonList(RandomStringUtils.randomAlphanumeric(10)))
+                .build();
+
+        Event event = Event.builder()
+                .build();
+
+        assertTrue(rule1.evaluate(event, upn, null, null, project1));
+        assertTrue(rule2.evaluate(event, upn, null, null, project1));
+        assertFalse(rule3.evaluate(event, upn, null, null, project1));
+
+        assertFalse(rule1.evaluate(event, upn, null, null, project2));
+        assertTrue(rule2.evaluate(event, upn, null, null, project2));
+        assertTrue(rule3.evaluate(event, upn, null, null, project2));
+    }
+
+    @Test
+    public void evaluateProjectStatus () {
+        Rule rule1 = Rule.builder().projectStatus(RunningStatus.INIT).build();
+        Rule rule2 = Rule.builder().build();
+
+        Project project1 = new Project().toBuilder()
+                .build();
+        project1.initStatus();
+        project1.start();
+
+        Project project2 = new Project().toBuilder()
+                .build();
+        project2.initStatus();
+
+        Event event = Event.builder()
+                .build();
+
+        assertFalse(rule1.evaluate(event, upn, null, null, project1));
+        assertTrue(rule2.evaluate(event, upn, null, null, project1));
+
+
+        assertTrue(rule1.evaluate(event, upn, null, null, project2));
+        assertTrue(rule2.evaluate(event, upn, null, null, project2));
 
     }
+
+    @Test
+    public void evaluatePhaseStatus() {
+        Rule rule1 = Rule.builder().phaseStatus(RunningStatus.INIT).build();
+        Rule rule2 = Rule.builder().build();
+
+        Phase phase1 = new Phase().toBuilder().build();
+        phase1.initStatus();
+        phase1.start();
+
+        Phase phase2 = new Phase().toBuilder().build();
+        phase2.initStatus();
+
+        Event event = Event.builder().build();
+
+        assertFalse(rule1.evaluate(event, upn, null, phase1, null));
+        assertTrue(rule2.evaluate(event, upn, null, phase1, null));
+
+        assertTrue(rule1.evaluate(event, upn, null, phase2, null));
+        assertTrue(rule2.evaluate(event, upn, null, phase2, null));
+
+    }
+
+    @Test
+    public void evaluateWorkItemStatus() {
+        Rule rule1 = Rule.builder().workItemStatus(RunningStatus.INIT).build();
+        Rule rule2 = Rule.builder().build();
+
+        WorkItem workItem1 = new WorkItem().toBuilder().build();
+        workItem1.initStatus();
+        workItem1.start();
+
+        WorkItem workItem2 = new WorkItem().toBuilder().build();
+        workItem2.initStatus();
+
+        Event event = Event.builder().build();
+
+        assertFalse(rule1.evaluate(event, upn, workItem1, null , null));
+        assertTrue(rule2.evaluate(event, upn, workItem1, null, null));
+
+        assertTrue(rule1.evaluate(event, upn, workItem2, null, null));
+        assertTrue(rule2.evaluate(event, upn, workItem2, null, null));
+
+    }
+
+    @Test
+    public void evaluateWorkItemOwner() {
+        Rule rule1 = Rule.builder().isWorkItemOwner(true).build();
+        Rule rule2 = Rule.builder().isWorkItemOwner(false).build();
+        Rule rule3 = Rule.builder().build();
+
+        WorkItem workItem1 = new WorkItem().toBuilder()
+                .owner(upn)
+                .build();
+
+        WorkItem workItem2 = new WorkItem().toBuilder().build();
+
+        Event event = Event.builder().build();
+
+        assertTrue(rule1.evaluate(event, upn, workItem1, null , null));
+        assertFalse(rule2.evaluate(event, upn, workItem1, null, null));
+        assertTrue(rule3.evaluate(event, upn, workItem1, null, null));
+
+        assertFalse(rule1.evaluate(event, upn, workItem2, null, null));
+        assertTrue(rule2.evaluate(event, upn, workItem2, null, null));
+        assertTrue(rule2.evaluate(event, upn, workItem2, null, null));
+
+    }
+
+    @Test
+    public void evaluateWorkItemAssignee() {
+        Rule rule1 = Rule.builder().isWorkItemAssignee(true).build();
+        Rule rule2 = Rule.builder().isWorkItemAssignee(false).build();
+        Rule rule3 = Rule.builder().build();
+
+        WorkItem workItem1 = new WorkItem().toBuilder()
+                .assignee(upn)
+                .build();
+
+        WorkItem workItem2 = new WorkItem().toBuilder().build();
+
+        Event event = Event.builder().build();
+
+        assertTrue(rule1.evaluate(event, upn, workItem1, null , null));
+        assertFalse(rule2.evaluate(event, upn, workItem1, null, null));
+        assertTrue(rule3.evaluate(event, upn, workItem1, null, null));
+
+        assertFalse(rule1.evaluate(event, upn, workItem2, null, null));
+        assertTrue(rule2.evaluate(event, upn, workItem2, null, null));
+        assertTrue(rule2.evaluate(event, upn, workItem2, null, null));
+
+    }
+
+    @Test
+    public void evaluateWorkItemMilestone() {
+        Rule rule1 = Rule.builder().isMilestone(true).build();
+        Rule rule2 = Rule.builder().isMilestone(false).build();
+        Rule rule3 = Rule.builder().build();
+
+        WorkItem workItem1 = new WorkItem().toBuilder()
+                .milestone(true)
+                .build();
+
+        WorkItem workItem2 = new WorkItem().toBuilder().build();
+
+        Event event = Event.builder().build();
+
+        assertTrue(rule1.evaluate(event, upn, workItem1, null , null));
+        assertFalse(rule2.evaluate(event, upn, workItem1, null, null));
+        assertTrue(rule3.evaluate(event, upn, workItem1, null, null));
+
+        assertFalse(rule1.evaluate(event, upn, workItem2, null, null));
+        assertTrue(rule2.evaluate(event, upn, workItem2, null, null));
+        assertTrue(rule2.evaluate(event, upn, workItem2, null, null));
+
+    }
+
 }
